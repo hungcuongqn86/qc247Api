1d69de0851aab01ab52672e0c9c273ee49f5812f
hungcuongqn86@gmail.com <hungcuongqn86@gmail.com>
2019-05-14 21:15:50 +0700

    chat

--- modules/Order/Http/Controllers/CommentController.php
+++ modules/Order/Http/Controllers/CommentController.php
@@ -8,7 +8,7 @@ use Modules\Order\Services\OrderServiceFactory;
 use Modules\Common\Services\CommonServiceFactory;
 use Modules\Common\Http\Controllers\CommonController;
 
-class HistoryController extends CommonController
+class CommentController extends CommonController
 {
     public function index()
     {
@@ -19,25 +19,7 @@ class HistoryController extends CommonController
     {
         $input = $request->all();
         try {
-            return $this->sendResponse(OrderServiceFactory::mHistoryService()->search($input), 'Successfully.');
-        } catch (\Exception $e) {
-            return $this->sendError('Error', $e->getMessage());
-        }
-    }
-
-    public function detail($id)
-    {
-        try {
-            return $this->sendResponse(OrderServiceFactory::mHistoryService()->findById($id), 'Successfully.');
-        } catch (\Exception $e) {
-            return $this->sendError('Error', $e->getMessage());
-        }
-    }
-
-    public function types()
-    {
-        try {
-            return $this->sendResponse(OrderServiceFactory::mHistoryService()->types(), 'Successfully.');
+            return $this->sendResponse(OrderServiceFactory::mCommentService()->search($input), 'Successfully.');
         } catch (\Exception $e) {
             return $this->sendError('Error', $e->getMessage());
         }
@@ -48,13 +30,11 @@ class HistoryController extends CommonController
         $input = $request->all();
         $arrRules = [
             'order_id' => 'required',
-            'type' => 'required',
             'content' => 'required'
         ];
         $arrMessages = [
             'order_id.required' => 'Không xác định được đơn hàng!',
-            'type.required' => 'Chưa chọn công việc thực hiện!',
-            'content.required' => 'Chưa nhập nội dung thực hiện!'
+            'content.required' => 'Chưa nhập nội dung!'
         ];
 
         $user = $request->user();
@@ -71,81 +51,7 @@ class HistoryController extends CommonController
                 return $this->sendError('Error', ['Đơn hàng không tồn tại!']);
             }
 
-            $userId = $order['order']['user_id'];
-            $debt = CommonServiceFactory::mTransactionService()->debt(['user_id' => $userId]);
-            $con_lai = 0;
-            $tongTien = 0;
-            if ($input['type'] == 5) {
-                $tongTien = $order['order']['tong'];
-                $arrPk = $order['order']['package'];
-                $tigia = $order['order']['rate'];
-                foreach ($arrPk as $pk) {
-                    if ($pk['ship_khach'] && $pk['ship_khach'] > 0) {
-                        $ndt = $pk['ship_khach'];
-                        $vnd = $ndt * $tigia;
-                        $tongTien = $tongTien + $vnd;
-                    }
-                }
-
-                $thanh_toan = empty($order['order']['thanh_toan']) ? 0 : $order['order']['thanh_toan'];
-                $con_lai = $tongTien - $thanh_toan;
-                if ($con_lai > $debt) {
-                    return $this->sendError('Error', ['Dư nợ không đủ để thanh lý!']);
-                }
-            }
-
-            $create = OrderServiceFactory::mHistoryService()->create($input);
-            if (!empty($create)) {
-                // Update order status
-                $orderInput = array();
-                $orderInput['id'] = $order['order']['id'];
-
-                if ($input['type'] == 4) {
-                    $orderInput['status'] = 4;
-                }
-                if ($input['type'] == 5) {
-                    $orderInput['status'] = 5;
-                    $orderInput['thanh_toan'] = $tongTien;
-
-                    // Transaction
-                    $transaction = [
-                        'user_id' => $userId,
-                        'type' => 6,
-                        'code' => $order['order']['id'] . '.H' . $create['id'],
-                        'value' => $con_lai,
-                        'debt' => $debt - $con_lai,
-                        'content' => $input['content']
-                    ];
-                    CommonServiceFactory::mTransactionService()->create($transaction);
-                }
-                if ($input['type'] == 6) {
-                    $orderInput['status'] = 6;
-                }
-                if ($input['type'] == 7) {
-                    $tiencoc = $order['order']['thanh_toan'];
-                    if (!empty($tiencoc) && $tiencoc > 0) {
-                        // Hoan tien
-                        $orderInput['datcoc_content'] = $input['content'];
-                        $orderInput['thanh_toan'] = 0;
-                        $orderInput['count_product'] = 0;
-                        $orderInput['tien_hang'] = 0;
-                        $orderInput['phi_tam_tinh'] = 0;
-                        $orderInput['tong'] = 0;
-
-                        // Transaction
-                        $transaction = [
-                            'user_id' => $userId,
-                            'type' => 5,
-                            'code' => $order['order']['id'] . '.H' . $create['id'],
-                            'value' => $tiencoc,
-                            'debt' => $debt + $tiencoc,
-                            'content' => $input['content']
-                        ];
-                        CommonServiceFactory::mTransactionService()->create($transaction);
-                    }
-                }
-                OrderServiceFactory::mOrderService()->update($orderInput);
-            }
+            $create = OrderServiceFactory::mCommentService()->create($input);
             return $this->sendResponse($create, 'Successfully.');
         } catch (\Exception $e) {
             return $this->sendError('Error', $e->getMessage());
