055d29c45fd7d916f7a80012fe8ec00c07a6335e
hungcuongqn86@gmail.com <hungcuongqn86@gmail.com>
2019-05-23 22:33:47 +0700

    package_code

--- modules/Order/Services/Impl/OrderService.php
+++ modules/Order/Services/Impl/OrderService.php
@@ -32,6 +32,12 @@ class OrderService extends CommonService implements IOrderService
                 $q->orWhere('phone_number', 'LIKE', '%' . $sKeySearch . '%');
             });
         }
+        $package_code = isset($filter['package_code']) ? trim($filter['package_code']) : '';
+        if (!empty($package_code)) {
+            $query->whereHas('Package', function ($q) use ($package_code) {
+                $q->where('package_code', '=', $package_code);
+            });
+        }
         $code = isset($filter['code']) ? trim($filter['code']) : '';
         if (!empty($code)) {
             $query->where('id', '=', $code);
