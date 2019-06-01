0bea87f0b260845b34eb31e7fa4e53fc3c7803f0
hungcuongqn86@gmail.com <hungcuongqn86@gmail.com>
2019-06-01 15:09:12 +0700

    bill

--- modules/Order/Http/Controllers/WarehouseController.php
+++ modules/Order/Http/Controllers/WarehouseController.php
@@ -51,61 +51,19 @@ class WarehouseController extends CommonController
         $billinput = array();
         $billinput['user_id'] = $input['user_id'];
         $billinput['employee'] = $user['id'];
-        $billinput['tong_can'] = 0;
-        $billinput['gia_can_nang'] = 0;
-        $billinput['tien_can'] = 0;
-        $billinput['tien_thanh_ly'] = 0;
         $billinput['status'] = 1;
         $billinput['so_ma'] = 0;
         try {
             //Lay danh sach kien hang
             $packages = OrderServiceFactory::mPackageService()->findByPkCodes($input['pkcodelist']);
-            $tongcan = 0;
             $soma = 0;
-            $orderIds = [];
             foreach ($packages as $package) {
-                $tongcan = $tongcan + $package['weight_qd'];
                 $soma = $soma + 1;
-                $orderIds[] = $package['order_id'];
                 if (!empty($package['bill_id'])) {
                     return $this->sendError('Error', ['Mã vận đơn đã được tạo ở phiếu xuất khác!']);
                 }
             }
-            $billinput['tong_can'] = $tongcan;
             $billinput['so_ma'] = $soma;
-            $gia_can_nang = 0;
-            if ($tongcan < 10) {
-                $gia_can_nang = 27000;
-            }
-            if (($tongcan >= 10) && ($tongcan <= 30)) {
-                $gia_can_nang = 23000;
-            }
-            if ($tongcan > 30) {
-                $gia_can_nang = 19000;
-            }
-            $billinput['gia_can_nang'] = $gia_can_nang;
-            $billinput['tien_can'] = $tongcan * $gia_can_nang;
-
-            // Lay danh sach order
-            $orders = OrderServiceFactory::mOrderService()->findByIds($orderIds);
-            $tienthanhly = 0;
-            foreach ($orders as $order) {
-                $tongTien = $order['tong'];
-                $arrPk = $order['package'];
-                $tigia = $order['rate'];
-                foreach ($arrPk as $pk) {
-                    if ($pk['ship_khach'] && $pk['ship_khach'] > 0) {
-                        $ndt = $pk['ship_khach'];
-                        $vnd = $ndt * $tigia;
-                        $tongTien = $tongTien + $vnd;
-                    }
-                }
-
-                $thanh_toan = empty($order['thanh_toan']) ? 0 : $order['thanh_toan'];
-                $con_lai = $tongTien - $thanh_toan;
-                $tienthanhly = $tienthanhly + $con_lai;
-            }
-            $billinput['tien_thanh_ly'] = $tienthanhly;
 
             // return $this->sendResponse($billinput, 'Successfully.');
             $create = OrderServiceFactory::mBillService()->create($billinput);
