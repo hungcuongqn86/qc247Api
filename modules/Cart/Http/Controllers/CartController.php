228bc71dfba348d0383b4429af0fd431024c9f0b
hungcuongqn86@gmail.com <hungcuongqn86@gmail.com>
2019-06-25 22:53:22 +0700

    them nhieu sp vao gio hang

--- modules/Cart/Http/Controllers/CartController.php
+++ modules/Cart/Http/Controllers/CartController.php
@@ -110,46 +110,50 @@ class CartController extends CommonController
     public function create(Request $request)
     {
         $input = $request->all();
-        $inputData = self::json_decode_nice($input['cart']);
-        $inputCart = (array)$inputData[0];
-        $arrRules = [
-            'amount' => 'required',
-            'domain' => 'required',
-            'image' => 'required',
-            'method' => 'required',
-            'name' => 'required',
-            'pro_link' => 'required',
-            'rate' => 'required',
-            'site' => 'required'
-        ];
-        $arrMessages = [
-            'amount.required' => 'amount.required',
-            'domain.required' => 'domain.required',
-            'image.required' => 'image.required',
-            'method.required' => 'method.required',
-            'name.required' => 'name.required',
-            'pro_link.required' => 'pro_link.required',
-            'rate.required' => 'rate.required',
-            'site.required' => 'site.required'
-        ];
-
-        $validator = Validator::make($inputCart, $arrRules, $arrMessages);
-        if ($validator->fails()) {
-            return $this->sendError('Error', $validator->errors()->all());
-        }
-
         try {
-            // Shop
-            $shop = ShopServiceFactory::mShopService()->findByUrl($inputCart['shop_link']);
-            if (!$shop) {
-                return $this->sendError('Error', 'Shop.' . $inputCart['shop_nick'] . '.NotExit');
+            $inputData = self::json_decode_nice($input['cart']);
+            foreach ($inputData as $item) {
+                $inputCart = (array)$item;
+                $arrRules = [
+                    'amount' => 'required',
+                    'domain' => 'required',
+                    'image' => 'required',
+                    'method' => 'required',
+                    'name' => 'required',
+                    'pro_link' => 'required',
+                    'rate' => 'required',
+                    'site' => 'required'
+                ];
+                $arrMessages = [
+                    'amount.required' => 'amount.required',
+                    'domain.required' => 'domain.required',
+                    'image.required' => 'image.required',
+                    'method.required' => 'method.required',
+                    'name.required' => 'name.required',
+                    'pro_link.required' => 'pro_link.required',
+                    'rate.required' => 'rate.required',
+                    'site.required' => 'site.required'
+                ];
+
+                $validator = Validator::make($inputCart, $arrRules, $arrMessages);
+                if ($validator->fails()) {
+                    return $this->sendError('Error', $validator->errors()->all());
+                }
+
+                // Shop
+                $shop = ShopServiceFactory::mShopService()->findByUrl($inputCart['shop_link']);
+                if (!$shop) {
+                    return $this->sendError('Error', 'Shop.' . $inputCart['shop_nick'] . '.NotExit');
+                }
+
+                $inputCart['shop_id'] = $shop['id'];
+                $user = $request->user();
+                $inputCart['user_id'] = $user->id;
+                $inputCart['price_arr'] = json_encode($inputCart['price_arr']);
+                $create = CartServiceFactory::mCartService()->create($inputCart);
             }
-            $inputCart['shop_id'] = $shop['id'];
-            $user = $request->user();
-            $inputCart['user_id'] = $user->id;
-            $inputCart['price_arr'] = json_encode($inputCart['price_arr']);
-            $create = CartServiceFactory::mCartService()->create($inputCart);
-            return $this->sendResponse($create, 'Successfully.');
+
+            return $this->sendResponse(1, 'Successfully.');
         } catch (\Exception $e) {
             return $this->sendError('Error', $e->getMessage());
         }
