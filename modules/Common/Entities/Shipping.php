c10de2cf7abaa079970a93030a15063dc9d3e7b5
cuongnh <hungcuongqn86@gmail.com>
2020-04-27 18:05:10 +0700

    shipping

--- modules/Common/Entities/Shipping.php
+++ modules/Common/Entities/Shipping.php
@@ -7,6 +7,10 @@ use Illuminate\Notifications\Notifiable;
 class Shipping extends BaseEntity
 {
     use Notifiable;
+	
+	const CHO_DUYET = 1;
+    const KHONG_DUYET = 2;
+    const DA_DUYET = 3;
 
     protected $table = 'shipping';
     protected $primaryKey = 'id';
@@ -21,9 +25,26 @@ class Shipping extends BaseEntity
         'created_at',
         'updated_at'
     ];
+	
+	protected $appends = ['statusname'];
+
+    public function getStatusnameAttribute()
+    {
+        $statusname = '';
+        if (!empty($this->attributes['status'])) {
+            $statusname = $this->list_of_status[$this->attributes['status']];
+        }
+        return $statusname;
+    }
 
     public function Order()
     {
         return $this->hasMany(Order::class, 'id', 'order_id');
     }
+	
+	public $list_of_status = [
+        self::CHO_DUYET => "Chờ xác nhận",
+        self::KHONG_DUYET => "Từ chối",
+        self::DA_DUYET => "Đã nhận"
+    ];
 }
