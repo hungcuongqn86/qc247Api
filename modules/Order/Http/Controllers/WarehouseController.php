0e06de3040d988d2687bdd34e6952a1aeb7bed91
hungcuongqn86 <hungcuongqn86@gmail.com>
2022-08-02 16:59:17 +0800

    fix bug

--- modules/Order/Http/Controllers/WarehouseController.php
+++ modules/Order/Http/Controllers/WarehouseController.php
@@ -116,6 +116,7 @@ class WarehouseController extends CommonController
         if ($bill['bill']['status'] == 2) {
             return $this->sendError('Error', ['Không thể xóa phiếu xuất đã xuất kho!']);
         }
+        DB::beginTransaction();
         try {
             // Package
             $billInput = array(
@@ -133,8 +134,10 @@ class WarehouseController extends CommonController
                     OrderServiceFactory::mPackageService()->update($packageInput);
                 }
             }
+            DB::commit();
             return $this->sendResponse($update, 'Successfully.');
         } catch (\Exception $e) {
+            DB::rollBack();
             return $this->sendError('Error', $e->getMessage());
         }
     }
